7634c0985ceedccbe7bd3fdf0995d0f0504c4791
Brian <controler12@gmail.com>
2015-08-22 13:21:07 -0400

    New files added. Uploading so Gerardo can take a look

--- ALUControl.vhd
+++ ALUControl.vhd
@@ -1,1 +1,18 @@
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.all;
+use IEEE.std_logic_signed.all;
 
+entity ALUControl is
+	port(	ALUOp : in std_logic_vector(1 downto 0);
+			ALUfunct : in std_logic_vector(5 downto 0);
+			ALUControl : out std_logic_vector(3 downto 0));
+	end ALUControl;
+
+architecture behavorial of ALUControl is
+  
+begin
+  
+  
+  
+  
+end behavorial;
